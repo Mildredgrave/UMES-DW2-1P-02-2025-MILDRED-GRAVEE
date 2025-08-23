a3890a235c8118a60e4a34cd0af3311f21abd379
Mildredgrave <mildredmejia2004@gmail.com>
2025-08-22 19:52:30 -0600

    cambios

--- examenes/routers/mochilas.route.js
+++ examenes/routers/mochilas.route.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import { 
+    createMochilaHandler,
   getMochilasHandler
 } from '../controllers/mochilas.controller.js';
 
 const router = Router();
 router.get('/', getMochilasHandler);
+router.post('/',createMochilaHandler)
 
 export default router;
